cde5524e86c983f5e0b43fe2c9a4bc756aae56fe
codes-abdullah <codes.abdullah@gmail.com>
2024-07-31 00:01:21 +0300

    v2

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 app_name='wiki'
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name='entry')
+    path("", views.index, name="index"),    
+    path("wiki/<str:title>", views.goto_entry, name='entry'),
+    path('create', views.create_entry, name='create')
 ]
 
 
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
-
+import markdown
+from django import forms
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def entry(request, title):
-    return render(request, f"encyclopedia/{title}.html")
+def goto_entry(request, title):    
+    entry_text = util.get_entry(title)
+    if entry_text == None:
+        return render(request, 'encyclopedia/not-found.html', {'entry':title});
+    html = markdown.markdown(entry_text, extras=['fenced-code-blocks'])
+    return render(request, 'encyclopedia/entry.html', {'entry_text':html})
+
+def create_entry(request):
+    return render(request, 'encyclopedia/create.html')
